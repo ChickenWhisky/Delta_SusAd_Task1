7aa99f7338b48b7768ad6d0135428e8ae061226d
thomas <thomasmampilli2003@gmail.com>
2023-05-07 00:23:58 +0530

    im trying my best somewhat

---
+++ reset.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo rm -r /home/Agate
+sudo rm -r /home/Opal
+sudo rm -r /home/GarnetA
+sudo rm -r /home/GarnetB
+
+sudo userdel Agate
+sudo userdel Opal
+sudo userdel GarnetA
+sudo userdel GarnetB
