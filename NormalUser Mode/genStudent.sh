5ce8cafb5c01480241c5d06aac9b50caf7a3f096
thomas <thomasmampilli2003@gmail.com>
2023-05-06 18:53:21 +0530

    pls work3

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -8,10 +8,10 @@ file="/home"
 if [ ! -d GarnetA ];
 then
 	
-	sudo useradd -m ${file}/GarnetA GarnetA
-	sudo useradd -m ${file}/GarnetB GarnetB
-	sudo useradd -m ${file}/Opal Opal
-	sudo useradd -m ${file}/Agate Agate
+	sudo useradd -m -d  ${file}/GarnetA GarnetA
+	sudo useradd -m -d  ${file}/GarnetB GarnetB
+	sudo useradd -m -d  ${file}/Opal Opal
+	sudo useradd -m -d  ${file}/Agate Agate
 	
 	room_number=(0 0 0 0)
 	room_occ_number=(0 0 0 0)
@@ -25,13 +25,13 @@ then
 		then
 			if [[ room_occ_number[0]=0 ]] 
 				then
-		    	sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student1 $1
+		    	sudo useradd -m -d ${file}/GarnetA/${room_number[0]}/Student1 $1
 				sudo touch ${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student1/fees.txt				
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
 				room_occ_number[0]=1
 			else
-			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student2 $1
+			    sudo useradd -m -d ${file}/GarnetA/${room_number[0]}/Student2 $1
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
@@ -46,13 +46,13 @@ then
 		then
 			if [[ room_occ_number[1]=0 ]] 
 				then
-			    sudo useradd -m ${file}/GarnetB/${room_number[1]}/Student1 $1
+			    sudo useradd -m -d ${file}/GarnetB/${room_number[1]}/Student1 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 				room_occ_number[1]=1
 		else
-			    sudo useradd -m ${file}/GarnetB/${room_number[1]}/Student2 $1
+			    sudo useradd -m -d ${file}/GarnetB/${room_number[1]}/Student2 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
@@ -66,13 +66,13 @@ then
 		then
 			if [[ room_occ_number[2]=0 ]] 
 				then
-			    sudo useradd -m ${file}/Opal/${room_number[2]}/Student1 $1
+			    sudo useradd -m -d ${file}/Opal/${room_number[2]}/Student1 $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m ${file}/Opal/${room_number[2]}/Student $1
+			    sudo useradd -m -d ${file}/Opal/${room_number[2]}/Student $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student2/userDetails.txt
@@ -86,13 +86,13 @@ then
 		then
 		if [[ room_occ_number[3]=0 ]] 
 			then
-			    sudo useradd -m ${file}/Agate/${room_number[3]}/Student1 $1
+			    sudo useradd -m -d ${file}/Agate/${room_number[3]}/Student1 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m ${file}/Agate/${room_number[3]}/Student2 $1
+			    sudo useradd -m -d ${file}/Agate/${room_number[3]}/Student2 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student2/userDetails.txt
@@ -108,7 +108,7 @@ fi
 #	value=$(awk '{print $2}' ${file}/src/User_Accounts.txt | sort | uniq)
 #	for i in $value
 #	do
-#		sudo useradd -m   ${file}/${i}/${i} ${i}_manager
+#		sudo useradd -m -d ${file}/${i}/${i} ${i}_manager
 #		sudo touch ${file}/${i}/${i}/Branch_Transaction_History.txt
 #		sudo touch ${file}/${i}/${i}/Branch_Current_Balance.txt
 #		sudo wget -O ${file}/${i}/${i}/Daily_Interest_Rates.txt inductions.delta.nitt.edu/sysad-task1 aily_Interest_Rates.txt
@@ -118,7 +118,7 @@ fi
 
 #if [[  $# -eq 5 ]];
 #then
-#	sudo useradd -m   ${file}/$2/$1 $1
+#	sudo useradd -m -d ${file}/$2/$1 $1
 #	sudo touch ${file}/$2/$1/Transaction_History.txt
 #	sudo echo "500">${file}/$2/$1/Current_Balance.txt
 #	sudo echo "$1 $2 $3 $4 $5">>${file}/src/User_Accounts.txt
