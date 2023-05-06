0c4f79aa3c405c4d6eaf613aff43531c5464e3f8
thomas <thomasmampilli2003@gmail.com>
2023-05-06 18:40:04 +0530

    pls work

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -25,19 +25,19 @@ then
 		then
 			if [[ room_occ_number[0]=0 ]] 
 				then
-		    	sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student1 "$1"
+		    	sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student1 $1
 				sudo touch ${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student1/fees.txt				
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
 				room_occ_number[0]=1
 			else
-			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student2 "$1"
+			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student2 $1
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
 
 				room_occ_number[0]=0
-				room_number[0]=awk [room_number[0]+1]
+				room_number[0]=awk [ $room_number[0]+1 ]
 		fi
 
 #Checks if the hostel is GarnetB then creates a new student user,its home directory,Userdetails.txt &fees.txt file
@@ -46,17 +46,18 @@ then
 		then
 			if [[ room_occ_number[1]=0 ]] 
 				then
-			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student1  "$1"
+			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student1 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 				room_occ_number[1]=1
 		else
-			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student2 "$1"
+			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student2 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
 				room_occ_number[1]=0
+				room_number[1]=awk [ $room_number[1]+1 ]
 		fi	
 
 #Checks if the hostel is Opal then creates a new student user,its home directory,Userdetails.txt &fees.txt file
@@ -65,35 +66,37 @@ then
 		then
 			if [[ room_occ_number[2]=0 ]] 
 				then
-			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student1 "$1"
+			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student1 $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student "$1"
+			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student2/userDetails.txt
-				room_occ_number[2]=0	
+				room_occ_number[2]=0
+				room_number[2]=awk [ $room_number[2]+1 ]	
 		fi
 
 #Checks if the hostel is Agate then creates a new student user,its home directory,Userdetails.txt &fees.txt file
 
-	else
+	elif [[ $3 == "Agate" ]]
 		if [[ room_occ_number[3]=0 ]] 
 			then
-			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student1 "$1"
+			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student1 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student2 "$1"
+			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student2 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student2/userDetails.txt
 				room_occ_number[3]=0
+				room_number[3]=awk [ $room_number[3]+1 ]
 		fi
 	fi	
 	done
