bf3881c9dbf6a5ae2908ad620c87f80b822cc307
thomas <thomasmampilli2003@gmail.com>
2023-05-07 00:52:10 +0530

    if this doesnt work go revert to the previous version

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -13,30 +13,29 @@ then
 	sudo useradd -m -d  /home/Opal Opal
 	sudo useradd -m -d  /home/Agate Agate
 	
-	room_number=(0 0 0 0)
 	room_occ_number=(0 0 0 0)
 
 #   value=$(awk '{print $1" "$2" "$3" "$4" "$5" "$6 }' /home/Delta_SusAd_Task1/NormalUser\ Mode/src/studentDetails.txt )
 	#!/bin/bash
-	while IFS=: read username fullname usergroups
+	while IFS=  read name rollno hostel room mess messpref
 	do
-    	useradd -G $usergroups -c "$fullname" $username
+    	
 #Checks if the hostel is GarnetA then creates a new student user,its home directory,Userdetails.txt &fees.txt file
 
-	if [ "$3" = "GarnetA" ]
+	if [ "$hostel" = "GarnetA" ]
 		then
 			if [[ room_occ_number[0] -eq 0 ]] 
 				then
-		    	sudo useradd -m -d /home/GarnetA/${room_number[0]}/Student1 $1
-				sudo touch /home/GarnetA/${room_number[0]}/Student1/userDetails.txt
-		    	sudo touch /home/GarnetA/${room_number[0]}/Student1/fees.txt				
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/GarnetA/${room_number[0]}/Student1/userDetails.txt
+		    	sudo useradd -m -d /home/GarnetA/$room/Student1 $name
+				sudo touch /home/GarnetA/$room/Student1/userDetails.txt
+		    	sudo touch /home/GarnetA/$room/Student1/fees.txt				
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/GarnetA/$room/Student1/userDetails.txt
 				room_occ_number[0]=1
 			else
-			    sudo useradd -m -d /home/GarnetA/${room_number[0]}/Student2 $1
-		    	sudo touch /home/GarnetA/${room_number[0]}/Student2/userDetails.txt
-		    	sudo touch /home/GarnetA/${room_number[0]}/Student2/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/GarnetA/${room_number[0]}/Student2/userDetails.txt
+			    sudo useradd -m -d /home/GarnetA/$room/Student2 $name
+		    	sudo touch /home/GarnetA/$room/Student2/userDetails.txt
+		    	sudo touch /home/GarnetA/$room/Student2/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/GarnetA/$room/Student2/userDetails.txt
 
 				room_occ_number[0]=0
 				room_number[0]= $($room_number[0]+1)
@@ -44,60 +43,60 @@ then
 
 #Checks if the hostel is GarnetB then creates a new student user,its home directory,Userdetails.txt &fees.txt file
 
-	elif [ "$3" = "GarnetB" ]
+	elif [ "$hostel" = "GarnetB" ]
 		then
 			if [[ room_occ_number[1] -eq 0 ]] 
 				then
-			    sudo useradd -m -d /home/GarnetB/${room_number[1]}/Student1 $1
-		    	sudo touch /home/GarnetB/${room_number[1]}/Student1/userDetails.txt
-		    	sudo touch /home/GarnetB/${room_number[1]}/Student1/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/GarnetB/${room_number[1]}/Student1/userDetails.txt
+			    sudo useradd -m -d /home/GarnetB/$room/Student1 $name
+		    	sudo touch /home/GarnetB/$room/Student1/userDetails.txt
+		    	sudo touch /home/GarnetB/$room/Student1/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/GarnetB/$room/Student1/userDetails.txt
 				room_occ_number[1]=1
 		else
-			    sudo useradd -m -d /home/GarnetB/${room_number[1]}/Student2 $1
-		    	sudo touch /home/GarnetB/${room_number[1]}/Student2/userDetails.txt
-		    	sudo touch /home/GarnetB/${room_number[1]}/Student2/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/GarnetB/${room_number[1]}/Student2/userDetails.txt
+			    sudo useradd -m -d /home/GarnetB/$room/Student2 $name
+		    	sudo touch /home/GarnetB/$room/Student2/userDetails.txt
+		    	sudo touch /home/GarnetB/$room/Student2/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/GarnetB/$room/Student2/userDetails.txt
 				room_occ_number[1]=0
 				room_number[1]= $($room_number[1]+1)
 		fi	
 
 #Checks if the hostel is Opal then creates a new student user,its home directory,Userdetails.txt &fees.txt file
 
-	elif [ "$3" = "Opal" ]
+	elif [ "$hostel" = "Opal" ]
 		then
 			if [[ room_occ_number[2] -eq 0 ]] 
 				then
-			    sudo useradd -m -d /home/Opal/${room_number[2]}/Student1 $1
-		    	sudo touch /home/Opal/${room_number[2]}/Student1/userDetails.txt
-		    	sudo touch /home/Opal/${room_number[2]}/Student1/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/Opal/${room_number[2]}/Student1/userDetails.txt
+			    sudo useradd -m -d /home/Opal/$room/Student1 $name
+		    	sudo touch /home/Opal/$room/Student1/userDetails.txt
+		    	sudo touch /home/Opal/$room/Student1/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/Opal/$room/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m -d /home/Opal/${room_number[2]}/Student $1
-		    	sudo touch /home/Opal/${room_number[2]}/Student2/userDetails.txt
-		    	sudo touch /home/Opal/${room_number[2]}/Student2/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/Opal/${room_number[2]}/Student2/userDetails.txt
+			    sudo useradd -m -d /home/Opal/$room/Student $name
+		    	sudo touch /home/Opal/$room/Student2/userDetails.txt
+		    	sudo touch /home/Opal/$room/Student2/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/Opal/$room/Student2/userDetails.txt
 				room_occ_number[2]=0
 				room_number[2]= $($room_number[2]+1)	
 		fi
 
 #Checks if the hostel is Agate then creates a new student user,its home directory,Userdetails.txt &fees.txt file
 
-	elif [ "$3" = "Agate" ]
+	elif [ "$hostel" = "Agate" ]
 		then
 		if [[ room_occ_number[3] -eq 0 ]] 
 			then
-			    sudo useradd -m -d /home/Agate/${room_number[3]}/Student1 $1
-		    	sudo touch /home/Agate/${room_number[3]}/Student1/userDetails.txt
-		    	sudo touch /home/Agate/${room_number[3]}/Student1/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/Agate/${room_number[3]}/Student1/userDetails.txt
+			    sudo useradd -m -d /home/Agate/$room/Student1 $name
+		    	sudo touch /home/Agate/$room/Student1/userDetails.txt
+		    	sudo touch /home/Agate/$room/Student1/fees.txt
+				sudo echo "$name $rollno $hostel $room $mess $messpref">>/home/Agate/$room/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m -d /home/Agate/${room_number[3]}/Student2 $1
-		    	sudo touch /home/Agate/${room_number[3]}/Student2/userDetails.txt
-		    	sudo touch /home/Agate/${room_number[3]}/Student2/fees.txt
-				sudo echo "$1 $2 $3 $4 $5 $6">>/home/Agate/${room_number[3]}/Student2/userDetails.txt
+			    sudo useradd -m -d /home/Agate/$room/Student2 $name
+		    	sudo touch /home/Agate/$room/Student2/userDetails.txt
+		    	sudo touch /home/Agate/$room/Student2/fees.txt
+				sudo echo "$$name $rollno $hostel $room $mess $messpref>>/home/Agate/$room/Student2/userDetails.txt
 				room_occ_number[3]=0
 				room_number[3]= $($room_number[3]+1)
 		fi
