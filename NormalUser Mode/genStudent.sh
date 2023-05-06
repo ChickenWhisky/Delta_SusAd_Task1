71d1a9e57c310eba76e9f528b1c2af3cc5d3147e
thomas <thomasmampilli2003@gmail.com>
2023-05-06 18:24:28 +0530

    lmao idk at this point im trying my best

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -16,7 +16,7 @@ then
 	room_number=(0 0 0 0)
 	room_occ_number=(0 0 0 0)
 
-	value=$(awk '{print $1" "$2}' ${file}/Delta_SusAd_Task1/NormalUser\ Mode/src/studentDetails.txt )
+	value=$(awk '{print $1" "$2" "$3" "$4" "$5" "$6 }' ${file}/Delta_SusAd_Task1/NormalUser\ Mode/src/studentDetails.txt )
 	for i in $value
 	do
 #Checks if the hostel is GarnetA then creates a new student user,its home directory,Userdetails.txt &fees.txt file
@@ -25,15 +25,13 @@ then
 		then
 			if [[ room_occ_number[0]=0 ]] 
 				then
-			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/
-		    	sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student1
+		    	sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student1 $1
 				sudo touch ${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
-		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student1/fees.txt
-				
+		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student1/fees.txt				
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student1/userDetails.txt
 				room_occ_number[0]=1
 			else
-			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/
+			    sudo useradd -m ${file}/GarnetA/${room_number[0]}/Student2 $1
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetA/${room_number[0]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetA/${room_number[0]}/Student2/userDetails.txt
@@ -48,13 +46,13 @@ then
 		then
 			if [[ room_occ_number[1]=0 ]] 
 				then
-			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/
+			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student1 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student1/userDetails.txt
 				room_occ_number[1]=1
 		else
-			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/
+			    sudo useradd -m   ${file}/GarnetB/${room_number[1]}/Student2 $1
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
 		    	sudo touch ${file}/GarnetB/${room_number[1]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/GarnetB/${room_number[1]}/Student2/userDetails.txt
@@ -67,13 +65,13 @@ then
 		then
 			if [[ room_occ_number[2]=0 ]] 
 				then
-			    sudo useradd -m   ${file}/Opal/${room_number[2]}/
+			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student1 $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m   ${file}/Opal/${room_number[2]}/
+			    sudo useradd -m   ${file}/Opal/${room_number[2]}/Student $1
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Opal/${room_number[2]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Opal/${room_number[2]}/Student2/userDetails.txt
@@ -85,13 +83,13 @@ then
 	else
 		if [[ room_occ_number[3]=0 ]] 
 			then
-			    sudo useradd -m   ${file}/Agate/${room_number[3]}/
+			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student1 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student1/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student1/userDetails.txt
 				room_occ_number[2]=1
 		else
-			    sudo useradd -m   ${file}/Agate/${room_number[3]}/
+			    sudo useradd -m   ${file}/Agate/${room_number[3]}/Student2 $1
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/userDetails.txt
 		    	sudo touch ${file}/Agate/${room_number[3]}/Student2/fees.txt
 				sudo echo "$1 $2 $3 $4 $5 $6">>${file}/Agate/${room_number[3]}/Student2/userDetails.txt
