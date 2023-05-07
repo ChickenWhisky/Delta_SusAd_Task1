d05e59834960f14621ae9f430a0b37bad8e8ab82
thomas <thomasmampilli2003@gmail.com>
2023-05-08 03:48:30 +0530

    just a no reason commit

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -27,8 +27,8 @@ getYear() {
 }
 addusers(){
  	sudo useradd -m -d /home/$hostel/$room/$name $name
-   	local department= $(getDepartment $rollno)
-	local year= $(getYear $rollno) 
+   	local department= $(getDepartment)
+	local year= $(getYear) 
 	sudo touch /home/$hostel/$room/$name/userDetails.txt
    	sudo touch /home/$hostel/$room/$name/fees.txt		
 	echo 'name rollno hostel year room mess allocated_mess month mess_preference' | sudo tee -a /home/$hostel/$room/$name/userDetails.txt 2>/dev/null
@@ -60,7 +60,6 @@ then
 
 	if [ "$hostel" = "GarnetA" ]
 		then
-		echo helloo
 		addusers
 
 #Checks if the hostel is GarnetB then creates a new student user,its home directory,Userdetails.txt &fees.txt file
