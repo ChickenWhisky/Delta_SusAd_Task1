5daa95bd58c8e7ec9872f63334c5b1579a49e991
thomas <thomasmampilli2003@gmail.com>
2023-05-12 01:30:08 +0530

    Made changes to genStudent for the case where the student isnt mentioned in the list

--- NormalUser Mode/genStudent.sh
+++ NormalUser Mode/genStudent.sh
@@ -64,14 +64,22 @@ then
 	done < /home/Delta_SusAd_Task1/NormalUser\ Mode/src/studentDetails.txt
 #Does the same as above except in this case we are adding student info after creation of hostel warden accounts
 else
-	while read -r name rollno hostel room mess messpref
-	do    	
+   	
  #Checks if the hostel valid then creates a new student user,its home directory,Userdetails.txt &fees.txt file
-	if [ "$hostel" = "GarnetA" ] || [ "$hostel" = "GarnetB" ] || [ "$hostel" = "Opal" ] ||  [ "$hostel" = "Agate" ]
+	if [ "$3" = "GarnetA" ] || [ "$3" = "GarnetB" ] || [ "$3" = "Opal" ] ||  [ "$3" = "Agate" ]
 		then
-		addusers
+		sudo useradd -m -d /home/$3/$4/$1 $1 >/dev/null;echo "$1:password" | sudo chpasswd
+   		getDepartment
+		getYear
+		sudo touch /home/$3/$4/$1/userDetails.txt
+   		sudo touch /home/$3/$4/$1/fees.txt		
+		echo 'name rollno department hostel year room allocated_mess month mess_preference' | sudo tee -a /home/$3/$4/$1/userDetails.txt >/dev/null
+		echo $1" "$2" "$department" "$3" "$year" "$4" "$5" - "$(date +%m)" "$6 | sudo tee -a /home/$3/$4/$1/userDetails.txt >/dev/null			
+		sudo cp /home/Delta_SusAd_Task1/NormalUser\ Mode/messAllocation.sh /home/$3/$4/$1/messAllocation.sh
+		sudo cp /home/Delta_SusAd_Task1/NormalUser\ Mode/feeBreakup.sh /home/$3/$4/$1/feeBreakup.sh
+		echo 'cumulativeAmountPaid= 0' | sudo tee -a /home/$3/$4/$1/fees.txt >/dev/null
+		echo 'User '$1' has been generated'
 	else 
 		continue
 	fi		
-	done < /home/Delta_SusAd_Task1/NormalUser\ Mode/src/studentDetails.txt
 fi	
