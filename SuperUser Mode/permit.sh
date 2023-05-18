ba3f4963bf051f11153f12cbef7e6b82a9f50f49
thomas <thomasmampilli2003@gmail.com>
2023-05-18 05:37:40 +0530

    made changes in permits.sh again

--- SuperUser Mode/permit.sh
+++ SuperUser Mode/permit.sh
@@ -28,7 +28,7 @@ for i in 'GarnetA' 'GarnetB' 'Opal' 'Agate'; do
 
     sudo chmod 700 /home/$i/signOutDefaulters.txt
     sudo chmod 700 /home/$i/signOutHistory.txt
-    sudo chmod 730 /home/$i/signOutRequests.txt
+    sudo chmod 740 /home/$i/signOutRequests.txt
 
     sudo setfacl -m "g:$i:r-x" /home/$i/announcements.txt
     sudo setfacl -m "g:$i:r-x" /home/$i/signOutDefaulters.txt
